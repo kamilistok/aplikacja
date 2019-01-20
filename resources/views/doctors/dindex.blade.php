ed37df395753277907d6462fc3a29220d2ec77ce
Kamil <kamil.m.istok@gmail.com>
2019-01-20 15:47:53 +0100

    20-01-2019

---
+++ resources/views/doctors/dindex.blade.php
@@ -0,0 +1,24 @@
+@extends('layout')
+
+@section('content')
+
+
+    <table class="table table-hover">
+        <tr>
+            <th>ID</th>
+            <th>Imie</th>
+            <th>Nazwisko</th>
+            <th>Specjalizacja</th>
+        </tr>
+
+        @foreach($doctors as $doctor)
+            <tr>
+                <td>{{$doctor->id}}</td>
+                <td>{{$doctor->name}}</td>
+                <td>{{$doctor->lastName}}</td>
+                <td>{{$doctor->specialization}}</td>
+            </tr>
+            @endforeach
+    </table>
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,8 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('doctors', 'DoctorsController');
 Route::group([
     'middleware' =>'roles',
     'roles' => ['Admin']
